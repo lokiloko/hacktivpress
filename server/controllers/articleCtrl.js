bdd276004e8227b7c63221ac6e641f0c6fa44d93
lokiloko <ianw.tkj2@gmail.com>
2017-11-09 13:51:04 +0700

    add read all article models and controllers

--- server/controllers/articleCtrl.js
+++ server/controllers/articleCtrl.js
@@ -1,5 +1,12 @@
+const Article = require('../models/article')
 class ArticleCtrl {
-
+  static read (req, res, next) {
+    Article.read().then((articles) => {
+      res.status(200).send(articles)
+    }).catch((err) => {
+      res.status(500).send(err)
+    })
+  }
 }
 
 module.exports = ArticleCtrl;
---
+++ server/models/article.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost/hacktivpress-ian', {
+  useMongoClient:true
+})
+
+mongoose.Promise = global.Promise
+
+var articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+var Article = mongoose.Model('Article', articleSchema)
+
+class Model {
+  static model () {
+    return Article
+  }
+  static read () {
+    return new Promise((resolve, reject) => {
+      Article.find({
+        $sort:{
+          created_at: -1
+        }
+      }).then((articles) => {
+        var obj = {
+          message: 'Data Found'
+          data: articles
+        }
+        resolve(obj)
+      }).catch((err) => {
+        var obj = {
+          message: 'Error when get data'
+          err: err
+        }
+        reject(err)
+      })
+    })
+  }
+  static create () {
+
+  }
+}
+module.exports = Model;
