e1cd12d7460240db359f7e66fa15a13c35506292
lokiloko <ianw.tkj2@gmail.com>
2017-11-09 14:18:12 +0700

    remove unused file. user controllers and user routes

--- server/controllers/userCtrl.js
+++
@@ -1,5 +0,0 @@
-class UserCtrl {
-
-}
-
-module.exports = UserCtrl;
--- server/routes/users.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const UserCtrl = require('../controllers/userCtrl')
-
-module.exports = router;
