2ce3a84334bf92acb1d10f2d4480fb99ad39a69a
lokiloko <ianw.tkj2@gmail.com>
2017-11-09 15:25:32 +0700

    setup mutation for store

--- client/src/vuex/mutation.js
+++ client/src/vuex/mutation.js
@@ -1,7 +1,12 @@
-const migrations = {
+const mutations = {
   toggleLog (state) {
     if (localStorage.getItem('token')) {
-      
+      state.loggedIn = true
+    } else {
+      state.loggedIn = false
     }
+  },
+  setArticles (state, payload) {
+    state.articles = payload
   }
 }
